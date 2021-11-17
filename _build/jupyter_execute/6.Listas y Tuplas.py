0bc2bcf1b4962f2096b72ad5e9f53853563ba88b
izainea <cizaineam@gmail.com>
2021-11-17 00:24:55 -0500

    rev4

--- _build/jupyter_execute/6.Listas y Tuplas.py
+++ _build/jupyter_execute/6.Listas y Tuplas.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Listas y Tuplas
+# # Listas (introducción)
 
 # :::::{important} ¿Cómo vamos?
 # 
@@ -62,7 +62,7 @@
 # In[1]:
 
 
-l = [0, 1, 2, 3]
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 l
 
 
@@ -120,7 +120,7 @@ l[2]
 # In[6]:
 
 
-l[-1]
+l[-7]
 
 
 # In[7]:
@@ -151,15 +151,15 @@ len(m)
 m[5]
 
 
-# In[12]:
+# In[11]:
 
 
-l[-5]
+l[-11]
 
 
 # En las listas podemos tener como índices números enteros, o expresiones que arrojen  un número entero, por ejemplo
 
-# In[13]:
+# In[12]:
 
 
 l[3] is l[2+1]
@@ -170,7 +170,7 @@ l[3] is l[2+1]
 # *(Días de la semana)* Una lista que contengan los días de la semana.
 # :::
 
-# In[3]:
+# In[13]:
 
 
 Dias=["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
@@ -183,13 +183,13 @@ Dias=["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"]
 # `FV=["Fresas","Papaya","Limón","Brocoli","Acelgas","Espinacas"]`
 # :::
 
-# In[4]:
+# In[14]:
 
 
-FV=["Fresas","Papaya","Limón","Brocoli","Acelgas","Espinacas"]
+FV=["Fresas","Papaya","Limón","Brócoli","Acelgas","Espinacas"]
 
 
-# In[5]:
+# In[15]:
 
 
 FV
@@ -197,7 +197,7 @@ FV
 
 # El contenido de las listas se puede modificar de manera muy sencilla, accedemos a la posición que deseamos modificar y asignamos el valor que deseamos esté allí, por ejemplo:
 
-# In[14]:
+# In[16]:
 
 
 m[0] = 1
@@ -208,48 +208,45 @@ m
 # 
 # Pero no todas las secuencias de objetos lo son, por ejemplo, al definir la cadena `s = 'hola'`, no podemos modificar el elemento `s[0]`:
 
-# In[15]:
+# In[17]:
 
 
 s = 'hola'
 
 
-# In[16]:
+# In[18]:
 
 
 s[0]
 
 
-# In[17]:
+# In[19]:
 
 
 s[0] = 'B'
 
 
-# In[ ]:
+# Una necesidad recurrente es: **agregar elementos a una lista**, para ello la forma más natural de hacerlo es mediante el método 
+# ```python
+# lista.append(elemento_a_agregar)
+# ```
 
-
-
-
-
-# Una necesidad recurrente es: **agregar elementos a una lista**, para ello la forma más natural de hacerlo es mediante el método `lista.append(elemento_a_agregar)`:
-
-# In[18]:
+# In[20]:
 
 
 l
 
 
-# In[19]:
+# In[21]:
 
 
-l.append('cuatro')
+l.append('diez')
 l
 
 
 # Las listas pueden concatenarse, es decir, dadas dos listas, obtendremos como resultado una nueva lista que tenga los elementos de la primera lista seguido de los de la segunda, así:
 
-# In[20]:
+# In[22]:
 
 
 M = l+m
@@ -258,118 +255,18 @@ M
 
 # Esto nos permite agregar elementos a una lista de otra manera, empleando el operador `+=`, como podemos ver:
 
-# In[21]:
+# In[24]:
 
 
-l += [5]
+l += [11]
 l
 
+
+# En otras palabras, para agregar elementos a una lista tenemos dos formas:
+# * Una empleando el método `lista.append(elemento_a_agregar)`.
+# * Otro por medio del operador, `+=` así: `lista += [elemento_a_agregar]`.
 
 # ### Ejercicio
 # 1. Crea un bloque de código que dada una lista en blanco vaya agregando elementos a ésta. Dichos elementos deberán ser la suma de los índices hasta la posición en que se halla el elemento.
 # 
 # 
-
-# En otras ocasiones es muy necesario eliminar elementos de las listas, por ejemplo, vamos a eliminar el segundo elemento de la lista `M` que creamos anteriormente:
-
-# In[22]:
-
-
-del M[2]
-M
-
-
-# Es decir, al seguir la línea de comando `del lista[índice_que_se_desea_eliminar]`, se borra la entrada de la lista.
-
-# ## Tuplas
-# Las tuplas, a diferencia de las listas, son objetos inmutables en los cuales se almacenan datos, bien sean del mismo o de diferentes tipos, siendo éste último el uso más frecuente.
-# 
-# De modo que definiremos nuestra primera tupa:
-
-# In[23]:
-
-
-temperatura = 'Enero', '01', 19
-#Equivalentemente
-#t = ('Enero', '01', 19)
-temperatura
-#Sin importar como la definamos, siempore se visualizarán sus elementos entre paréntesis y separados por una coma
-
-
-# Para conocer la cantidad de elementos que hay en una tupla, empleamos la misma función que para las listas, la función `len(tupla)`, así:
-
-# In[24]:
-
-
-len(temperatura)
-
-
-# Si deseamos definir una tupla con un único elemento, debemos hacerlo de la siguiente manera:
-
-# In[25]:
-
-
-tupla1elemento = 23, #Observa la coma
-tupla1elemento
-
-
-# La coma es la que indica que es una tupla, ya que se si hace sin ella, incluso entre paréntesis, se guardará el entero 23 y no la tupla cuyo único elemento es el 23.
-# 
-# Para acceder a la información almacenada en las diferentes entradas de una tupla, lo hacemos de la misma manera que lo hicimos con las listas, es decir, `tupla[índice]`, así:
-
-# In[26]:
-
-
-temperatura[0]
-
-
-# y éstas se pueden operar:
-
-# In[27]:
-
-
-'La temperatura máxima en Bogotá el '+ temperatura[1]+' de '+temperatura[0]+' del 2021 fue de '+str(temperatura[-1])
-
-
-# ¿Qué pasa si intentamos modificar una entrada de una tupla?
-
-# In[28]:
-
-
-temperatura[0]='hola'
-
-
-# Como lo comentamos antes, las tuplas son **inmutables**.
-# 
-# Para agregar elementos a una tupla empleamos el operador `+=`, de la misma manera que lo hicimos con las listas. Para las tuplas no disponemos del método `append`.
-
-# In[29]:
-
-
-tupla1elemento += ('a',)
-tupla1elemento
-
-
-# Como hemos recalcado las tuplas son inmutables, pero pueden contener elementos mutables, por ejemplo:
-
-# In[30]:
-
-
-tupla1elemento += ([1,2,3,4],)
-tupla1elemento
-
-
-# In[31]:
-
-
-tupla1elemento[2]=1
-#genererá un error ya que no podemos modificar el elemento de la tupla
-
-
-# In[34]:
-
-
-#Pero podemos modificar la lista que está dentro de la tupla
-tupla1elemento[2][3]='cuatro'
-tupla1elemento
-
